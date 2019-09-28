4f8edb3db15cc351bd171afeb13bf6294f9247d9
毛才君 <mccj@MCCJ-PC>
2019-09-28 19:53:12 +0800

    1

--- Hangfire.Dashboard.Management/Pages/JobDetailsPage.cs
+++ Hangfire.Dashboard.Management/Pages/JobDetailsPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hangfire.Annotations;
+using System;
+using System.Collections.Generic;
 
 namespace Hangfire.Dashboard.Management.Pages
 {
@@ -20,11 +22,30 @@ namespace Hangfire.Dashboard.Management.Pages
     {
         public static NonEscapedString Render(Common.Job job)
         {
-            if (job == null) { return new NonEscapedString("<em>" + Hangfire.Dashboard.Resources.Strings.Common_CannotFindTargetMethod + "</em>"); }
+            //if (job == null) { return new NonEscapedString("<em>" + Hangfire.Dashboard.Resources.Strings.Common_CannotFindTargetMethod + "</em>"); }
 
             var type = Type.GetType("Hangfire.Dashboard.JobMethodCallRenderer,Hangfire.Core");
             var render = type.GetMethod("Render");
             return render.Invoke(null, new[] { job }) as NonEscapedString;
+        }
+    }
+    internal class RecurringJobEntity
+    {
+        public static void ParseCronExpression([NotNull] string cronExpression)
+        {
+            var type = Type.GetType("Hangfire.RecurringJobEntity,Hangfire.Core");
+            var render = type.GetMethod("ParseCronExpression");
+            var cc = render.Invoke(null, new[] { cronExpression });
+        }
+    }
+    internal static class ContinuationsSupportAttributeEx
+    {
+        internal static List<Continuation> DeserializeContinuations(string serialized)
+        {
+            var type = Type.GetType("Hangfire.ContinuationsSupportAttribute,Hangfire.Core");
+            var render = type.GetMethod("DeserializeContinuations", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static);
+            return render.Invoke(null, new[] { serialized }) as List<Continuation>;
+
         }
     }
 }
